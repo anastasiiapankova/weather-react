71c44788c489a2a551b1d54212af6bef0dfe7ce2
anastasiiapankova <anastasiapankova25@gmail.com>
2022-10-27 16:43:54 +0300

    Updated icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -23,14 +23,12 @@ export default function WeatherIcon(props) {
     "50n": "FOG",
   };
 
-  if (props.code === "01d") {
-    return (
-      <ReactAnimatedWeather
-        icon={codeMapping[props.code]}
-        color="#edbd33"
-        size={52}
-        animate={true}
-      />
-    );
-  }
+  return (
+    <ReactAnimatedWeather
+      icon={codeMapping[props.code]}
+      color="#edbd33"
+      size={52}
+      animate={true}
+    />
+  );
 }
